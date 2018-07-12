2dc0b08dfc9c2b4b5dcb62921536b48c5833b87b
straumat <stephane.traumat@gmail.com>
2018-07-12 23:34:15 +0200

    Adding checkstyle

--- src/main/java/com/oakinvest/cerise/Application.java
+++ src/main/java/com/oakinvest/cerise/Application.java
@@ -3,10 +3,19 @@ package com.oakinvest.cerise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application.
+ */
+@SuppressWarnings({"PMD", "checkstyle:hideutilityclassconstructor"})
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+    /**
+     * Application launcher.
+     *
+     * @param args parameters.
+     */
+	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
---
+++ src/main/java/com/oakinvest/cerise/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Application.
+ */
+package com.oakinvest.cerise;
